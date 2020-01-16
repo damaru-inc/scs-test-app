dceae9343dfaf402219b2c48b1b54ecf6ad086e6
Michael Davis <michael@damaru.com>
2020-01-16 17:22:59 -0500

    Working now with dynamic destinations.

--- src/main/java/com/solace/scs/test/ScsTestAppApplication.java
+++ src/main/java/com/solace/scs/test/ScsTestAppApplication.java
@@ -1,10 +1,7 @@
 package com.solace.scs.test;
 
-import com.solace.scs.example.FahrenheitTempSource;
+import com.solace.scs.example.Messaging;
 import com.solace.scs.example.SensorReading;
-import com.solace.scs.gen.CelsiusChannel;
-import com.solace.scs.gen.FahrenheitChannel;
-import com.solace.scs.gen.TempReading;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,68 +9,107 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
 
 @SpringBootApplication
+//@EnableBinding
 @ComponentScan("com.solace.scs")
+@Configuration
 public class ScsTestAppApplication implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(ScsTestAppApplication.class);
 
-//	@Autowired
-//	FahrenheitTempSource fahrenheitTempSource;
-
 	@Autowired
-	FahrenheitChannel fahrenheitChannel;
-
-	@Autowired
-	CelsiusChannel celsiusChannel;
-
+	private Messaging messaging;
 
 	public static void main(String[] args) {
-		SensorReading sr = new SensorReading();
-		SpringApplication.run(ScsTestAppApplication.class, args);
+		SpringApplication.run(ScsTestAppApplication.class);
 	}
 
-//	public void receive(SensorReading sensorReading) {
-//		log.info("Got one! " + sensorReading);
-//	}
-	public void receiveF(TempReading tempReading) {
-		double t = tempReading.getTemperature();
-		log.info("Got F! " + tempReading.getBaseUnit() + " " + t);
-		TempReading tr = new TempReading();
-		tr.setTemperature((t - 32) * 5.0 / 9.0);
-		tr.setBaseUnit(TempReading.BaseUnit.CELSIUS);
-		celsiusChannel.send(tr);
-	}
-
-	public void receiveC(TempReading tempReading) {
-		log.info("Got C! " + tempReading.getBaseUnit() +  " " + tempReading.getTemperature());
-	}
 	@Override
 	public void run(String... args) throws Exception {
-		fahrenheitChannel.setCallback(this::receiveF);
-		celsiusChannel.setCallback(this::receiveC);
-
-		for (int i = 0; i < 1; i++) {
-			TempReading tr = new TempReading();
-			tr.setBaseUnit(TempReading.BaseUnit.FAHRENHEIT);
-			tr.setSensorId("sensor 0");
-			tr.setTemperature( 212.0 );
-			fahrenheitChannel.send(tr);
-		}
-
-		Thread.sleep(1000);
+		runExample();
 	}
 
-	//	@Override
-//	public void run(String... args) throws Exception {
-//		fahrenheitTempSource.setCallback(this::receive);
+	private void runExample() throws Exception {
+		messaging.setCallback(this::receive);
+
+		boolean run = true;
+
+		while (run) {
+			SensorReading sensorReading = emitSensorReading();
+			messaging.send(sensorReading);
+			Thread.sleep(900);
+		}
+
+	}
+
+	public void receive(SensorReading sensorReading) {
+		log.info("Got one! " + sensorReading);
+	}
+
+	// copied from scs-example:
+	private static final Random random = new Random(System.currentTimeMillis());
+	private static final int RANDOM_MULTIPLIER = 100;
 //
-//		while (true) {
-//			SensorReading sensorReading = fahrenheitTempSource.emitSensorReading();
-//			log.info("Sending " + sensorReading);
-//			fahrenheitTempSource.sendIt(sensorReading);
-//			Thread.sleep(1000);
-//		}
+	public SensorReading emitSensorReading() {
+		double temperature = random.nextDouble() * RANDOM_MULTIPLIER;
+		temperature = Math.floor(temperature);
+
+		SensorReading reading = new SensorReading();
+		reading.setSensorID("sensor1");
+		reading.setTemperature(temperature);
+		reading.setBaseUnit(SensorReading.BaseUnit.FAHRENHEIT);
+
+		//log.info("Emitting: " + reading);
+
+		return reading;
+	}
+//
+//
+//	// FROM THE LIBRARY
+//	private final EmitterProcessor<Message<?>> processor = EmitterProcessor.create();
+//
+//	Consumer<SensorReading> callback;
+//
+//	@Bean
+//	public Supplier<Flux<Message<?>>> supplier() {
+//		log.info("sensorReadingSupplier called.");
+//		return () -> processor;
 //	}
+	//@Bean
+//	public Supplier<Message<?>> supplier1() {
+//		log.info("sensorReadingSupplier1 called.");
+//		return () -> buildMessage(emitSensorReading());
+//	}
+
+//	@Bean
+//	public Consumer<SensorReading> consumer() {
+//		log.info("sensorReadingConsumer called.");
+//		return s -> callback.accept(s);
+//	}
+//
+//	public void setCallback(Consumer<SensorReading> callback) {
+//		this.callback = callback;
+//	}
+//
+//	public void send(SensorReading sensorReading) {
+//		Message<?> message = buildMessage(sensorReading);
+//		log.info("Sending------------  " + sensorReading);
+//		processor.onNext(message);
+//	}
+//
+//	private Message<?> buildMessage(SensorReading sensorReading) {
+//		String topic = "sensor-t";
+//		//String topic = "sensor/" + topicIndex++;
+//		Message<SensorReading> message = MessageBuilder
+//				.withPayload(sensorReading)
+//				.setHeader("spring.cloud.stream.sendto.destination", topic)
+//				.build();
+//		return message;
+//	}
+
+
 }
